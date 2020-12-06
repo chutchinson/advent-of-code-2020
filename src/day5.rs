283b919873ba7d7217bfeae5c4a9011c77273d80
Chris Hutchinson <chris@cshutchinson.com>
2020-12-05 19:27:14 -0500

    simplified

--- src/day5.rs
+++ src/day5.rs
@@ -1,32 +1,24 @@
 mod prelude;
 use prelude::*;
 
-fn bsp(range: Range<usize>, spec: &str) -> usize {
-     spec.chars().fold(range, |x, ch| {
-            let n = (x.end - x.start) / 2;
-            match ch {
-                'F' | 'L' => x.start..x.end - n - 1,
-                'B' | 'R' => x.start + n + 1..x.end,
-                _ => x
-            }
-        })
-        .start
-}
-
-fn parse(spec: &str) -> (usize, usize, usize) {
-    let row = bsp(0..127, &spec[0..7]);
-    let col = bsp(0..7, &spec[7..]);
-    (row, col, row * 8 + col)
+fn bsp(spec: &str) -> (usize, usize, usize) {
+     let n = spec.chars().fold(0, |x, ch| {
+         x << 1 | match ch {
+            'F' | 'L' => 0,
+             _ => 1
+         }
+     });
+     (n >> 3, n & 7, n)
 }
 
 fn main() {
     let assignments = stdin().lock().lines()
         .filter_map(Result::ok)
-        .map(|line| parse(&line).2)
+        .map(|line| bsp(&line).2)
         .sorted()
         .collect_vec();
 
-    let max_seat_id = *assignments.last().unwrap();
+    let max_seat_id = assignments.last().unwrap();
 
     println!("{}", max_seat_id);
 
@@ -44,8 +36,8 @@ mod tests {
 
     #[test]
     fn example_1() {
-        assert_eq!(parse("BFFFBBFRRR"), (70, 7, 567));
-        assert_eq!(parse("FFFBBBFRRR"), (14, 7, 119));
-        assert_eq!(parse("BBFFBBFRLL"), (102, 4, 820));
+        assert_eq!(bsp("BFFFBBFRRR"), (70, 7, 567));
+        assert_eq!(bsp("FFFBBBFRRR"), (14, 7, 119));
+        assert_eq!(bsp("BBFFBBFRLL"), (102, 4, 820));
     }
 }
